3f8584042c67435b250b2518c71d512670efa8be
hugua <1047810633@qq.com>
2016-12-26 14:05:04 +0800

    增加了redis工具类

--- src/main/java/lab/io/rush/dao/TicketDaoImpl.java
+++ src/main/java/lab/io/rush/dao/TicketDaoImpl.java
@@ -2,7 +2,9 @@ package lab.io.rush.dao;
 
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
 import javax.jdo.Query;
 import javax.jdo.Transaction;
 
@@ -14,28 +16,33 @@ import lab.io.rush.util.MyPmUtil;
 @Component("TicketDao")
 public class TicketDaoImpl  implements TicketDao{
 	
-	
+	@Resource(name="myPmf")
+	private PersistenceManagerFactory pmf;
 	public Film getfilmByid(String filmid) {
-		PersistenceManager pm = MyPmUtil.getPm();
+		PersistenceManager pm = pmf.getPersistenceManager();
 		Query q = pm.newQuery("SELECT FROM " + Film.class.getName() +" WHERE id == "+ filmid);
 		q.setUnique(true);
 		Film film= (Film)q.execute();
+		pm.close();
 		return film;
 	}
 
 
 	public List<Film> findSellingTicket() {
-		PersistenceManager pm = MyPmUtil.getPm();
+//		PersistenceManager pm = MyPmUtil.getPm();
+		PersistenceManager pm = pmf.getPersistenceManager();
 		Query q = pm.newQuery("SELECT FROM " + Film.class.getName()+" WHERE number > 0") ;
 		List<Film> films= (List<Film>)q.execute();	
+		pm.close();
 		return films;
 	}
 
 
 	public List<Film> findTicket() {
-		PersistenceManager pm = MyPmUtil.getPm();
+		PersistenceManager pm = pmf.getPersistenceManager();
 		Query q = pm.newQuery("SELECT FROM " + Film.class.getName()) ;
 		List<Film> films= (List<Film>)q.execute();	
+		pm.close();
 		return films;
 	}
 }
--- src/main/java/lab/io/rush/dao/UserDaoImpl.java
+++ src/main/java/lab/io/rush/dao/UserDaoImpl.java
@@ -2,7 +2,9 @@ package lab.io.rush.dao;
 
 import java.util.List;
 
+import javax.annotation.Resource;
 import javax.jdo.PersistenceManager;
+import javax.jdo.PersistenceManagerFactory;
 import javax.jdo.Query;
 import javax.jdo.Transaction;
 
@@ -13,10 +15,13 @@ import lab.io.rush.util.MyPmUtil;
 @Component("UserDao")
 public class UserDaoImpl implements UserDao{
 
+	@Resource(name="myPmf")
+	private PersistenceManagerFactory pmf;
 	public int login(String username, String password) {
-		 PersistenceManager pm = MyPmUtil.getPm();
+		 PersistenceManager pm = pmf.getPersistenceManager();
 	     Query q = pm.newQuery("SELECT FROM " + User.class.getName()+"  WHERE name == '"+username+"'"+"&& password =='" + password+"'");
          List<User> holders = (List<User>) q.execute();
+         pm.close();
          if(holders.size() > 0){
          	return 1;
          }
@@ -24,17 +29,18 @@ public class UserDaoImpl implements UserDao{
 	}
 
 	public User findUserByname(String username) {
-		 PersistenceManager pm = MyPmUtil.getPm();
+		PersistenceManager pm = pmf.getPersistenceManager();
 	     Query q = pm.newQuery("SELECT FROM " + User.class.getName()+"  WHERE name == '"+username+"'");
 	     q.setUnique(true);
 	     User user = (User) q.execute();
+	     pm.close();
 	     return user;
 
 	}
 
 	public int regist(String mail, String username, String password) {
 		int msg ;
-		PersistenceManager pm = MyPmUtil.getPm();
+		PersistenceManager pm = pmf.getPersistenceManager();
 		User user = new User(username,password,mail);
 
 		Query q = pm.newQuery("SELECT FROM " + User.class.getName()+"  WHERE name == '"+username+"'");
@@ -46,6 +52,7 @@ public class UserDaoImpl implements UserDao{
 			pm.makePersistent(user);
 			
 		}
+		pm.close();
 		return msg;
 	}
 }
