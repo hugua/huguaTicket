25e8b72fc627403eda31b787f48aa933d7081d8e
hugua <1047810633@qq.com>
2017-01-06 09:25:37 +0800

    修改了qq第三方登录

--- src/main/java/lab/io/rush/controller/qqloginController.java
+++ src/main/java/lab/io/rush/controller/qqloginController.java
@@ -7,18 +7,22 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.qq.connect.QQConnectException;
 import com.qq.connect.oauth.Oauth;
 
 @Component
 @RequestMapping("/qqlogin.do")
 public class qqloginController {
+
 	@RequestMapping
-	public ModelAndView handleRequest(HttpServletRequest request,HttpServletResponse response) throws Exception {
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
         try {
             response.sendRedirect(new Oauth().getAuthorizeURL(request));
-        } catch (Exception e) {
+        } catch (QQConnectException e) {
             e.printStackTrace();
         }
-		return null;
+        
+        return null;
 	}
 }
+
